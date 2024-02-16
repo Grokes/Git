0c0ba70ec4da294cf7e1fae2e7355675377501ca
Grokes <default1one@gmail.com>
2024-02-16 14:40:02 +0300

    Добавление данных

--- Program.cs
+++ Program.cs
@@ -9,28 +9,34 @@ namespace Git
     {
         static void Main()
         {
-            if (!File.Exists("GoodInvest.db"))
+            if (File.Exists("GoodInvest.db"))
             {
-                using (var connection = new SqliteConnection("Data Source=GoodInvest.db"))
-                {
-                    connection.Open();
-
-                    SendSQLCommandNonQuery(connection, @"CREATE TABLE Investors (
-                                                    [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                                    [Name] TEXT(50) NOT NULL,
-                                                    [Investment] INT NOT NULL,
-                                                    [age] int NOT NULL);
-                                                    CREATE TABLE Companies (
-                                                    [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                                    [Name] TEXT(50) NOT NULL,
-                                                    [Investment] INT NOT NULL);
-                                                    CREATE TABLE GrowthOfShares (
-                                                    [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                                    [CompanyId] INT NOT NULL,
-                                                    [Percent] REAL NOT NULL);");
-                }
+                File.Delete("GoodInvest.db");
             }
+            using (var connection = new SqliteConnection("Data Source=GoodInvest.db"))
+            {
+                connection.Open();
 
+                SendSQLCommandNonQuery(connection, @"CREATE TABLE Investors (
+                                                [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                                [Name] TEXT(50) NOT NULL,
+                                                [Investment] INT NOT NULL,
+                                                [age] int NOT NULL);
+                                                CREATE TABLE Companies (
+                                                [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                                [Name] TEXT(50) NOT NULL,
+                                                [Investment] INT NOT NULL);
+                                                CREATE TABLE GrowthOfShares (
+                                                [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                                [CompanyId] INT NOT NULL,
+                                                [Percent] REAL NOT NULL);");
+                SendSQLCommandNonQuery(connection, @"INSERT INTO Companies(Name, Investment)
+                                                    VALUES ('Google', 1000), ('Yandex', 500), ('Mail.ru', 100);
+                                                    INSERT INTO Investors(Name, Investment, age)
+                                                    VALUES ('Dmitriy', 1000, 30), ('Alexey', 500, 40), ('Victor', 100, 22);
+                                                    INSERT INTO GrowthOfShares(CompanyId, Percent)
+                                                    VALUES (1, 10), (2, 20), (3, 30);");
+            }
         }
 
         private static void SendSQLCommandNonQuery(SqliteConnection connection, string textCommand)
