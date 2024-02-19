848bce8edfaad39b4fcc6b3aff5c7ecf1dc9e4ed
Grokes <default1one@gmail.com>
2024-02-19 16:00:34 +0300

    Бд удалена с помощью EF

--- Program.cs
+++ Program.cs
@@ -9,37 +9,37 @@ namespace Git
     {
         static void Main()
         {
-            if (File.Exists("GoodInvest.db"))
-            {
-                File.Delete("GoodInvest.db");
-            }
+            //if (File.Exists("GoodInvest.db"))
+            //{
+            //    File.Delete("GoodInvest.db");
+            //}
 
-            var connectionString = new SqliteConnectionStringBuilder();
-            connectionString.DataSource = "GoodInvest.db";
-            using (var connection = new SqliteConnection(connectionString.ConnectionString))
-            {
-                connection.Open();
+            //var connectionString = new SqliteConnectionStringBuilder();
+            //connectionString.DataSource = "GoodInvest.db";
+            //using (var connection = new SqliteConnection(connectionString.ConnectionString))
+            //{
+            //    connection.Open();
 
-                SendSQLCommandNonQuery(connection, @"CREATE TABLE Investors (
-                                                [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                                [Name] TEXT(50) NOT NULL,
-                                                [Investment] INT NOT NULL,
-                                                [age] int NOT NULL);
-                                                CREATE TABLE Companies (
-                                                [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                                [Name] TEXT(50) NOT NULL,
-                                                [Investment] INT NOT NULL);
-                                                CREATE TABLE GrowthOfShares (
-                                                [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                                [CompanyId] INT NOT NULL,
-                                                [Percent] REAL NOT NULL);");
-                SendSQLCommandNonQuery(connection, @"INSERT INTO Companies(Name, Investment)
-                                                    VALUES ('Google', 1000), ('Yandex', 500), ('Mail.ru', 100);
-                                                    INSERT INTO Investors(Name, Investment, age)
-                                                    VALUES ('Dmitriy', 1000, 30), ('Alexey', 500, 40), ('Victor', 100, 22);
-                                                    INSERT INTO GrowthOfShares(CompanyId, Percent)
-                                                    VALUES (1, 10), (2, 20), (3, 30);");
-            }
+            //    SendSQLCommandNonQuery(connection, @"CREATE TABLE Investors (
+            //                                    [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+            //                                    [Name] TEXT(50) NOT NULL,
+            //                                    [Investment] INT NOT NULL,
+            //                                    [age] int NOT NULL);
+            //                                    CREATE TABLE Companies (
+            //                                    [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+            //                                    [Name] TEXT(50) NOT NULL,
+            //                                    [Investment] INT NOT NULL);
+            //                                    CREATE TABLE GrowthOfShares (
+            //                                    [Id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+            //                                    [CompanyId] INT NOT NULL,
+            //                                    [Percent] REAL NOT NULL);");
+            //    SendSQLCommandNonQuery(connection, @"INSERT INTO Companies(Name, Investment)
+            //                                        VALUES ('Google', 1000), ('Yandex', 500), ('Mail.ru', 100);
+            //                                        INSERT INTO Investors(Name, Investment, age)
+            //                                        VALUES ('Dmitriy', 1000, 30), ('Alexey', 500, 40), ('Victor', 100, 22);
+            //                                        INSERT INTO GrowthOfShares(CompanyId, Percent)
+            //                                        VALUES (1, 10), (2, 20), (3, 30);");
+            //}
         }
 
         private static void SendSQLCommandNonQuery(SqliteConnection connection, string textCommand)
